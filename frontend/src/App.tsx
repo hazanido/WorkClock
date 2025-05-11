56019a088effab01eaf75988577746056e8d4195
hazanido <93769535+hazanido@users.noreply.github.com>
2025-05-11 18:34:03 +0300

    add user page and admin page for test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate  } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import UserPage from './pages/UserPage';
+import AdminPage from './pages/AdminPage';
+import { useAuth } from './context/AuthContext';
+
 
 function App() {
+
+  const { token, role } = useAuth();
+
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/user" element={token && role === 'user' ? <UserPage /> : <Navigate to="/" />}/>
+      <Route path="/admin" element={token && role === 'admin' ? <AdminPage /> : <Navigate to="/" />}/>
     </Routes>
   );
 }
